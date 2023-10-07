2553453f376bba360a67b149c7336b20440353c7
MahbubulRishad <mahbubulrishad@gmail.com>
2023-10-08 01:43:58 +0600

    Identifier and Data Type

---
+++ src/java_basic/class01/DataTypeTest.java
@@ -0,0 +1,19 @@
+package java_basic.class01;
+
+public class DataTypeTest {
+    public static void main(String[] args) {
+        int age = 20;
+        long number = 78945612312l;
+        double constantValue = 200.54;
+        float salary = 4500.50f;
+        boolean hasIdCard = true;
+        char firstLetter = 'A';
+
+        System.out.println("Int: " +age);
+        System.out.println("number: " +number);
+        System.out.println("constantValue: " +constantValue);
+        System.out.println("salary: " +salary);
+        System.out.println("hasIdCard: " +hasIdCard);
+        System.out.println("firstLetter: " +firstLetter);
+    }
+}
