8b1fc40aec22d4a8e1d5c069c4117149622ea212
MahbubulRishad <mahbubulrishad@gmail.com>
2023-10-18 00:15:03 +0600

    Methods

---
+++ src/java_basic/class03/TypesOfMethod/ParameterizedMethod.java
@@ -0,0 +1,20 @@
+package java_basic.class03.TypesOfMethod;
+
+public class ParameterizedMethod {
+    String name;
+    int age;
+
+    public void printHeading(){
+        System.out.println("Welcome to java world");
+    }
+
+    public String getName(String name) {
+        this.name = name;
+        return name;
+    }
+
+    public int getAge(int age) {
+        this.age = age;
+        return age;
+    }
+}
